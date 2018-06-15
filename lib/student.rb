68215600c41f4d13dcb6f08dcd7d08501a20a96a
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-06-14 20:21:56 -0400

    updating objects and DB

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,55 @@
 require_relative "../config/environment.rb"
+require 'pry'
+  class Student
+  attr_accessor :name, :grade
+  attr_reader :id
 
-class Student
+  def initialize(name, grade, id = nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
+  def self.create_table
+    sql = "CREATE TABLE students (id INTEGER PRIMARY KEY, name TEXT, grade INTEGER);"
+    DB[:conn].execute(sql)
+  end
 
+  def self.drop_table
+    sql = "DROP TABLE students"
+    DB[:conn].execute(sql)
+  end
 
-end
+  def save
+    if self.id
+      self.update
+
+    else
+    sql = "INSERT INTO students (name, grade) VALUES (?, ?)"
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
+  end
+
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
+
+  def self.create(name, grade)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
+
+  def self.new_from_db(row)
+    self.new(row[1], row[2], row[0])
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM students WHERE name = ?"
+    row = DB[:conn].execute(sql, name).flatten
+     self.new_from_db(row)
+  end
+
+  end
